ebd911c6a4b8099d2dccdc29035d20e065ea9e07
Roman Novák <bejvas.jod@gmail.com>
2013-03-21 16:47:21 +0100

    init

---
+++ Default.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <label>Server:</label>
+        <br />
+        <asp:TextBox ID="server" runat="server" />
+        <br />
+        <label>Shared secret:</label>
+        <br />
+        <asp:TextBox ID="secret" runat="server" />
+        <br />
+        <br />
+        <label>Login:</label>
+        <br />
+        <asp:TextBox ID="login" runat="server" />
+        <br />
+        <label>Passcode:</label>
+        <br />
+        <asp:TextBox ID="password" runat="server" />
+        <br />
+        <asp:Button ID="send" runat="server" Text="Login" />
+        <asp:Label ID="info" runat="server" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Ais.Net.Radius;
+using Ais.Net.Radius.Attributes;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        send.Click += send_Click;
+    }
+
+    void send_Click(object sender, EventArgs e)
+    {
+        string host = Dns.GetHostName();
+        IPHostEntry ip = Dns.GetHostEntry(host);
+        string nasIp = ip.AddressList[0].ToString();
+        IPAddress[] addrlist = Dns.GetHostAddresses(server.Text);
+
+        var client = new Client(addrlist[0], 1812, secret.Text)
+        {
+            SendTimeout = 5000,
+            ReceiveTimeout = 5000,
+            Ttl = 50
+        };
+
+        var request = new AccessRequest(nasIp, ServiceType.Framed, login.Text, password.Text, client);
+
+        var response = client.Send(request, true, 3);
+
+        info.Text = response.Packet.PacketType.ToString();
+    }
+}
